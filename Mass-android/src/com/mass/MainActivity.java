12c21b9fcf19f9a1c575bfd1c264547d712f3e12
Pollux <yuriy.kosakivsky@gmail.com>
2013-04-07 12:37:47 +0300

    new

---
+++ Mass-android/src/com/mass/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mass;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mass.Mass;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        //cfg.useAccelerometer = true;
+        cfg.useWakelock = true;
+        initialize(new Mass(), cfg);
+    }
+}
